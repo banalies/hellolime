f92cf1d9696f6388ab44d760d1db8b4f914f76a7
banalies <bannikov777@yandex.ru>
2017-11-30 18:56:24 +0300

    site

---
+++ HelloLime.com/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Главная</title>
+    <link rel="stylesheet" href="css/hw.css" type="text/css"/>
+	</head>
+	<body>
+  <header><a id="hwhead">HelloLime </a> <a href="reg.php" id="areg">регистрация</a> <a href="log.php" id="alog">вход</a> </header>
+	 <div id="blocklist"></div>
+	 	 <div id="toggle">click</div>
+	 <div id="panel"></div>
+
+    
+    
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+
+	 <script type="text/javascript" src="js/hw.js"></script>
+	</body>
+</html>
